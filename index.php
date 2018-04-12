00022ae9619464e72259e078e799af2e39a1c2a0
Mary <schmitt.mary7@gmail.com>
2018-04-12 14:12:37 -0600

    finally got missmarymac correctly deployed! time to play!

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>mary</title>
+</head>
+<body>
+<h1>beepboop</h1>
+<p>why wont this work i'm going nuts.</p>
+
+</body>
+</html>
